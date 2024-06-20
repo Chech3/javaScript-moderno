390c9280f1fa9a0971fde3b26131bbf47721e443
Chech3 <joseangelgarces123@gmail.com>
2024-06-20 08:38:48 -0400

    logica booleana

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -1,10 +1,24 @@
 let hoy = new Date(); //Fecha completa
-let dia = hoy.getDay(); //Devuelve el dia actual 
+// let dia = hoy.getDay(); //Devuelve el dia actual 
 console.log(hoy);
-console.log({dia});
 
-if (dia === 1) {
-    console.log("Es Lunes");
-} else {
-    console.log("No es Lunes");
+// if (dia === 1) {
+    //     console.log("Es Lunes");
+    // } else {
+        //     console.log("No es Lunes");
+        // }
+        
+        let dia = 0;
+        
+        // console.log({dia});
+const diasLetras = {
+    0: () => {"Domingo", console.log(2+10)},
+    1: () => "Lunes",
+    2: () => "Martes",
+    3: () => "Miercoles",
+    4: () => "Jueves",
+    5: () => "Viernes",
+    6: () => "Sabado"
 }
+
+console.log(diasLetras[dia]() || "Dia no valido");
